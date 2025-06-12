4d92ad99dc9cf02d81ba9a335f8244d05d860976
Mlynarz00 <ornat-marcin@wp.pl>
2025-06-12 18:24:25 +0200

    Uruchom bt
    Sprawdzanie czy BT jest uruchomiony, prośba jeśli nie jest, powiększenie guzików w Home

--- app/src/main/java/com/example/androidplanowaniewycieczek/ui/firstpage/SynchronizedActivity.java
+++ app/src/main/java/com/example/androidplanowaniewycieczek/ui/firstpage/SynchronizedActivity.java
@@ -1,11 +1,17 @@
 package com.example.androidplanowaniewycieczek.ui.firstpage;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,6 +24,30 @@ public class SynchronizedActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 2);
+                    return;
+                }
+                return;
+            }
+            startActivity(enableBtIntent);
+        }
+
+
 
         setContentView(R.layout.activity_synchronized);
         ImageView quitButton = findViewById(R.id.quit_button);
